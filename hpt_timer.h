7adb54f58e62d847ea5003c0ecdad331121e9031
SylwBar <sylw.bar@gmail.com>
2014-10-14 00:33:47 +0200

    HPT timer simple scheduling

--- hpt_timer.h
+++ hpt_timer.h
@@ -12,9 +12,24 @@ extern "C" {
 /* -------- defines -------- */
 #define HPT_MS(x) (x)
 
+typedef enum
+{
+   HPT_END      = 1, /* End the table */
+   HPT_RESTART,      /* Restart the table */
+   HPT_GPIO_UP,      /* Test GPIO up */
+   HPT_GPIO_DOWN     /* Test GPIO down */
+} htp_opcodes;
+
+/* -------- structures ------- */
+typedef struct
+{
+   uint32_t    time;
+   htp_opcodes opcode;
+} HPT_Event;
+
 /* -------- functions -------- */
 void HPT_Config(void);
-void HPT_Start(void);
+void HPT_Start(HPT_Event* hpt_table);
 
 #ifdef __cplusplus
 }
